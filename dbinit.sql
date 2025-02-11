8200f32e0387f01beeeeeef17db50d8ccdd736a6
Archis Gore <archisgore@meta.com>
2025-02-11 07:59:43 +0100

    Format

--- dbinit.sql
+++ dbinit.sql
@@ -3,10 +3,12 @@ CREATE TABLE
         id SERIAL PRIMARY KEY,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE NOT NULL
-            DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
-        updated_at TIMESTAMP WITH TIME ZONE NOT NULL
-            DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
-        reset_password_expires TIMESTAMP,
-        reset_password_token VARCHAR(255)
+        created_at TIMESTAMP
+        WITH
+            TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+            updated_at TIMESTAMP
+        WITH
+            TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+            reset_password_expires TIMESTAMP,
+            reset_password_token VARCHAR(255)
     );
